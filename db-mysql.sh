c29bb7006c15f38b9844ac4b63de862e3b2126eb
AvinDevops <avinashbandela73@gmail.com>
2026-08-28 00:41:31 +0530

    calling otherscript and using set

--- db-mysql.sh
+++ db-mysql.sh
@@ -10,7 +10,7 @@ systemctl start mysqld &>>$LOGFILE
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 # VALIDATE $? "Setting password for root"
-mysql -h db.aviexpense.online -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h 172.31.46.27 -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
